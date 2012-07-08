3cea9b98f398757793dd77e2896a160fd3d69265
Vlad Pranskevichus <vlprans@gmail.com>
2012-07-09 02:14:42 +0300

    Merge macos branch

--- personal/my-appearance.el
+++ personal/my-appearance.el
@@ -1,2 +1,4 @@
-(set-face-attribute 'default nil :height 120)
+(if (eq system-type 'darwin)
+    (set-face-attribute 'default nil :height 120)
+    (set-face-attribute 'default nil :height 90))
 (scroll-bar-mode 0)
