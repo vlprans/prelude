4d92bd7d47c80473dbdfc230f998fae0c2ec8047
Vlad Pranskevichus <vlad.pranskevichus@anahoret.com>
2013-07-11 16:20:05 +0300

    Enhanced-ruby-mode

--- personal/my-ruby.el
+++ personal/my-ruby.el
@@ -1,19 +1,18 @@
 (add-to-list 'load-path (expand-file-name (concat prelude-vendor-dir "emacs-rails/")))
 (add-to-list 'load-path (expand-file-name (concat prelude-vendor-dir "rhtml/")))
-(add-to-list 'auto-mode-alist '("\\.rake$" . ruby-mode))
-(add-to-list 'auto-mode-alist '("Rakefile$" . ruby-mode))
-(add-to-list 'auto-mode-alist '("\\.gemspec$" . ruby-mode))
-(add-to-list 'auto-mode-alist '("Gemfile$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.rb$" . enh-ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.rake$" . enh-ruby-mode))
+(add-to-list 'auto-mode-alist '("Rakefile$" . enh-ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.gemspec$" . enh-ruby-mode))
+(add-to-list 'auto-mode-alist '("Gemfile$" . enh-ruby-mode))
+(add-to-list 'auto-mode-alist '("Berksfile$" . enh-ruby-mode))
+(add-to-list 'auto-mode-alist '("Vagrantfile$" . enh-ruby-mode))
 
 (add-to-list 'auto-mode-alist '("\\.js\\.erb$" . javascript-mode))
 (add-to-list 'auto-mode-alist '("\\.coffee\\.erb$" . coffee-mode))
 
-(add-to-list 'load-path (concat prelude-vendor-dir "Enhanced-Ruby-Mode"))
-(setq ruby-program "~/.rbenv/versions/1.9.3-p194/bin/ruby")
-(setq enh-ruby-program "~/.rbenv/versions/1.9.3-p194/bin/ruby")
-
-(require 'ruby-mode)
-(require 'ruby-tools)
+(setq ruby-program "~/.rbenv/shims/ruby")
+(setq enh-ruby-program "~/.rbenv/shims/ruby")
 
 (require 'rsense)
 (require 'auto-complete)
