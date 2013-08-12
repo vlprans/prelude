f6593faefce464525025205307dca5fd34f18615
Vlad Pranskevichus <vlprans@gmail.com>
2013-08-12 08:10:27 +0300

    Add hsenv

--- personal/my-haskell.el
+++ personal/my-haskell.el
@@ -1,4 +1,5 @@
 (require 'hamlet-mode)
+(require 'hsenv)
 
 (add-to-list 'auto-mode-alist '(".xmobarrc" . haskell-mode))
 (add-to-list 'auto-mode-alist '(".lhs" . haskell-mode))
@@ -9,3 +10,5 @@
             (turn-on-haskell-decl-scan)
             (turn-on-haskell-doc-mode)
             (ghc-init)))
+
+(add-hook 'inferior-haskell-mode-hook 'turn-on-ghci-completion)
