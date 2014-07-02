629e8afeaad7c512b1110ab82d4e957d91050463
Vlad Pranskevichus <vlprans@gmail.com>
2014-07-02 11:17:36 +0300

    Some Haskell stuff

--- personal/my-haskell.el
+++ personal/my-haskell.el
@@ -1,8 +1,10 @@
 (require 'hamlet-mode)
-(require 'hsenv)
 
 (add-to-list 'auto-mode-alist '(".xmobarrc" . haskell-mode))
 (add-to-list 'auto-mode-alist '(".lhs" . haskell-mode))
+
+(autoload 'ghc-init "ghc" nil t)
+(autoload 'ghc-debug "ghc" nil t)
 
 (add-hook 'haskell-mode-hook
           (lambda ()
@@ -23,3 +25,23 @@
      (define-key haskell-mode-map (kbd "C-c C-i") 'haskell-process-do-info)
      (define-key haskell-mode-map (kbd "C-c M-.") nil)
      (define-key haskell-mode-map (kbd "C-c C-d") nil)))
+
+(defvar cabal-use-sandbox t)
+
+(defun cabal-toggle-sandboxing-local ()
+  (interactive)
+  (set (make-local-variable 'cabal-use-sandbox) (not cabal-use-sandbox))
+  (message (format "This buffer haskell-process-type is ``%s''"
+                   (set (make-local-variable 'haskell-process-type)
+                        (if cabal-use-sandbox
+                            'cabal-repl
+                          'ghci)))))
+
+(defun cabal-toggle-sandboxing ()
+  (interactive)
+  (setq cabal-use-sandbox (not cabal-use-sandbox))
+  (message (format "haskell-process-type is ``%s''"
+                   (setq haskell-process-type
+                         (if cabal-use-sandbox
+                             'cabal-repl
+                           'ghci)))))
