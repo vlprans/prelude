0bb303df8774a284756d460ec2e61fc8e4f11171
Vlad Pranskevichus <vlprans@gmail.com>
2016-02-23 00:46:38 +0200

    clj stuff

--- personal/my-clojure.el
+++ personal/my-clojure.el
@@ -1,3 +1,3 @@
-(add-hook 'clojure-mode-hook (lambda ()
-                               (cider-mode)
-                               (highlight-parentheses-mode)))
+;; (add-hook 'clojure-mode-hook (lambda ()
+;;                                (cider-mode)
+;;                                (highlight-parentheses-mode)))
