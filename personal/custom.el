6b17edd3210f56ed3b3a3802cf922dffa8ae7f41
Vlad Pranskevichus <vlprans@gmail.com>
2016-03-04 16:29:44 +0200

    clojure tweaks

---
+++ personal/custom.el
@@ -0,0 +1,15 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("0c311fb22e6197daba9123f43da98f273d2bfaeeaeb653007ad1ee77f0003037" "8db4b03b9ae654d4a57804286eb3e332725c84d7cdab38463cb6b97d5762ad26" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
+ '(ensime-inf-cmd-template (quote ("sbt" "console"))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
--- personal/my-clojure.el
+++ personal/my-clojure.el
@@ -1,3 +1,15 @@
-;; (add-hook 'clojure-mode-hook (lambda ()
-;;                                (cider-mode)
-;;                                (highlight-parentheses-mode)))
+(require 'clojure-mode)
+
+(add-hook 'clojure-mode-hook (lambda ()
+                               (clj-refactor-mode 1)))
+
+
+(define-clojure-indent
+  (defroutes 'defun)
+  (GET 2)
+  (POST 2)
+  (PUT 2)
+  (DELETE 2)
+  (HEAD 2)
+  (ANY 2)
+  (context 2))
